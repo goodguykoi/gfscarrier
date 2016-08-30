750207a19ff1b2b92edd4247b8ab0ccfaea24726
Nico Alina <angeloalina@gmail.com>
2016-08-30 19:44:51 +0800

    test

--- app/controllers/receive_sms_controller.rb
+++ app/controllers/receive_sms_controller.rb
@@ -1,12 +1,20 @@
 class ReceiveSmsController < ApplicationController
 
+  def spam
+    1.upto(params[:number].to_i) do |i|
+      uri = URI.parse("http://devapi.globelabs.com.ph/smsmessaging/v1/outbound/#{params[:senderAddress]}/requests")
+      uri.query = "access_token=#{params[:access_token]}"
+      @response = Net::HTTP.post_form(uri, {"address" => params[:address], "message" => "mga nakain ni erika: "+i.to_s})
+      logger.info i
+    end
 
+  end
 
   def receive
     if !params[:inboundsmsmessagelist].nil?
       #ito yung code bahala ka na dito
-      @message = params[:inboundsmsmessagelist][:inboundSMSMessage][:message]
-      render html: 'hello, world!' + @message
+      @message = params[:message]
+      spam
     end
   end
 end
